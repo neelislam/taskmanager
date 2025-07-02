f397e68820b785b000ef6e6845ef789024fd639a
rabiu <rabiul17.ri@gmail.com>
2025-07-02 12:35:09 +0600

    Added splash screen and navigated to Sign in screen

--- lib/ui/screen/splash_screen.dart
+++ lib/ui/screen/splash_screen.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:taskmanager/ui/screen/sign_in_screen.dart';
+import 'package:taskmanager/ui/utils/asset_paths.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,13 +12,36 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+  void initState(){
+    super.initState();
+    _moveToNextScreen(); // ashlei 2 sec wait kore ei function k auto call dibe
+
+
+  }
+
+
+  Future<void> _moveToNextScreen() async{
+    await Future.delayed(Duration(seconds: 3)); // 2 sec wait kore onno screen e jabe
+    //kivabe? Navigator push use kroe
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SignInScreen()));
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       body: Stack(
         children: [
-          SvgPicture.asset('assets/images/logo.svg'),
-          SvgPicture.asset('assets/images/background.svg'),
+          SvgPicture.asset(AssetPaths.backgroundSvg, fit: BoxFit.cover,
+            height: double.maxFinite,
+            width: double.maxFinite,),
+          Align(
+              alignment: Alignment.center,
+              child: SvgPicture.asset(AssetPaths.logoSvg)),
+
         ],
       ),
     );
--- lib/ui/utils/asset_paths.dart
+++ lib/ui/utils/asset_paths.dart
@@ -1,5 +1,8 @@
 class AssetPaths{
-
+static const String _imagePath = 'assets/images';
+static const String logoSvg = '$_imagePath/logo.svg';
+static const String backgroundSvg = '$_imagePath/screen-back.svg';
+static const String background2Svg = '$_imagePath/background.svg';
 
 
 }
