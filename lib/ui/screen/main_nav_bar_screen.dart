cf25a3951c609da0f9e41e4734a09f3e39395522
rabiu <rabiul17.ri@gmail.com>
2025-07-08 23:52:51 +0600

    main nav bar

---
+++ lib/ui/screen/main_nav_bar_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainNavBarScreen extends StatefulWidget {
+  const MainNavBarScreen({super.key});
+
+  @override
+  State<MainNavBarScreen> createState() => _MainNavBarScreenState();
+}
+
+class _MainNavBarScreenState extends State<MainNavBarScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
