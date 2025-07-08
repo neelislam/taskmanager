86a3be3f47014441f8d052cd07402b098b2b8aa0
rabiu <rabiul17.ri@gmail.com>
2025-07-09 00:34:40 +0600

    main nav bar

--- lib/ui/screen/main_nav_bar_screen.dart
+++ lib/ui/screen/main_nav_bar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taskmanager/ui/screen/new_task_list_screen.dart';
 
 class MainNavBarScreen extends StatefulWidget {
   const MainNavBarScreen({super.key});
@@ -9,12 +10,13 @@ class MainNavBarScreen extends StatefulWidget {
 }
 
 class _MainNavBarScreenState extends State<MainNavBarScreen> {
- List<Widget> _screens = [];
+ final List<Widget> _screens = [ NewTaskListScreen()];
  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: TaskManagerAppBar(),
       body: _screens[_selectedIndex],
       bottomNavigationBar:
       NavigationBar(
@@ -40,4 +42,55 @@ class _MainNavBarScreenState extends State<MainNavBarScreen> {
       ),
     );
   }
+}
+
+class TaskManagerAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const TaskManagerAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.greenAccent,
+      title: Row(
+        children: [
+          CircleAvatar(),
+          const SizedBox(width: 14,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Neel Islam',
+                style:
+                  TextStyle(
+                    fontSize: 16,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+
+                  ),
+                ),
+                Text('neelislam@gmail.com',
+                  style:
+                  TextStyle(
+                    fontSize: 14,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+
+                  ),
+                ),
+              ]
+
+            ),
+          ),
+          IconButton(onPressed: (){},  icon: Icon(Icons.logout),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => Size.fromHeight(kToolbarHeight); //Will keep a fixed height for the appbar in every screen
 }
