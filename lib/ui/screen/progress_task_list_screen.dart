9ff7bff218c12f20320bf0a1c999b4c36cd99dac
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-07-10 17:59:12 +0600

    Just Add last two page (similar)

--- lib/ui/screen/progress_task_list_screen.dart
+++ lib/ui/screen/progress_task_list_screen.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
-
 import '../widgets/task_card.dart';
-
-
 class ProgressTaskScreen extends StatefulWidget {
   const ProgressTaskScreen({super.key});
-
   @override
   State<ProgressTaskScreen> createState() => _ProgressTaskScreenState();
 }
-
 class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
   @override
   Widget build(BuildContext context) {
