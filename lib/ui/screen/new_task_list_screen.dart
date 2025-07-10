f17bd23b5802e1e6e4563b97bc9559a35e8c9cdc
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-07-10 12:36:49 +0600

    new task list | 101 UI D IV

--- lib/ui/screen/new_task_list_screen.dart
+++ lib/ui/screen/new_task_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/task_counter_widgets.dart';
+
 class NewTaskListScreen extends StatefulWidget {
   const NewTaskListScreen({super.key});
 
@@ -10,15 +12,32 @@ class NewTaskListScreen extends StatefulWidget {
 class _NewTaskListScreenState extends State<NewTaskListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Card(
-        elevation: 0,
-        child: Column(
-          children: [
-
-          ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(children: [
+        const SizedBox(height: 14,),
+        SizedBox(
+          height: 100,
+          child: ListView.separated(itemBuilder: (context, index) {
+            return TaskCounterWidgets(
+              title: 'Progress',
+              count: 12,
+            );
+          },
+              separatorBuilder: (context, index)=>const SizedBox(width: 1,),
+              itemCount: 5,
+          scrollDirection: Axis.horizontal,),
         ),
-      )
-    ]);
+        Expanded(child: ListView.builder(
+          itemCount: 10,
+            itemBuilder: (context, index)
+        {
+          return SizedBox();
+        }
+        )
+        )
+      ]),
+    );
   }
 }
+
---
+++ lib/ui/widgets/task_counter_widgets.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class TaskCounterWidgets extends StatelessWidget {
+  const TaskCounterWidgets({
+    super.key, required this.title, required this.count,
+  });
+
+  final String title;
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('$count',
+              style:
+              Theme.of(context).textTheme.titleLarge,),
+            Text(title),
+          ],
+        ),
+      ),
+    );
+  }
+}
