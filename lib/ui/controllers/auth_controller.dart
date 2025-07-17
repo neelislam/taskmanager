a7afd88ba15be1910ff4e4ba70006149f66dbdb7
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-07-17 23:46:45 +0600

    API Integration iv || 51 min

---
+++ lib/ui/controllers/auth_controller.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:taskmanager/data/models/user_model.dart';
+
+class AuthController{
+  static UserModel? userModel;
+  static String? accessToken;
+
+
+
+  static Future<void> saveUserData(UserModel model, Stream token) async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    await sharedPreferences.setString('User-data', jsonEncode(model.toJson()));
+    await sharedPreferences.setString('token', token as String);
+  userModel = model;
+  accessToken = token as String?;
+
+  }
+  static Future<void> getUserData() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+     userModel = UserModel.fromJson(
+         jsonDecode(sharedPreferences
+             .getString('User-data')!));
+     sharedPreferences.getString('token');
+  }
+
+
+}
