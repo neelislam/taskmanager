7eac7d4b0f248f336d4266997dc7ce97a55351a1
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-07-21 14:16:16 +0600

    Next: TaskManager Finish Up

--- lib/data/urls.dart
+++ lib/data/urls.dart
@@ -4,6 +4,7 @@ class Urls{
   static const String loginUrl = '$_baseUrl/Login';
   static const String createNewTaskUrl = '$_baseUrl/createNewTask';
   static const String getNewTaskUrl = '$_baseUrl/ListTaskByStatus/New';
+  static const String getProgressTaskUrl = '$_baseUrl/ListTaskByStatus/Progress';
 
 
 }
--- lib/ui/screen/progress_task_list_screen.dart
+++ lib/ui/screen/progress_task_list_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import '../../data/models/task_model.dart';
+import '../../data/service/network_caller.dart';
+import '../../data/urls.dart';
+import '../widgets/snack_bar_message.dart';
 import '../widgets/task_card.dart';
 class ProgressTaskScreen extends StatefulWidget {
   const ProgressTaskScreen({super.key});
@@ -6,16 +10,56 @@ class ProgressTaskScreen extends StatefulWidget {
   State<ProgressTaskScreen> createState() => _ProgressTaskScreenState();
 }
 class _ProgressTaskScreenState extends State<ProgressTaskScreen> {
+  bool _getProgressTaskInProgress = false;
+  List<TaskModel> _progressTaskList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getProgressTaskList();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12),
-      child: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          // return TaskCard(taskType: TaskType.progress,);
-        },
+      child: Visibility(
+        visible: _getProgressTaskInProgress == false,
+        replacement: Center(
+          child: CircularProgressIndicator(),
+        ),
+        child: ListView.builder(
+          itemCount: _progressTaskList.length,
+          itemBuilder: (context, index) {
+            return TaskCard(taskType: TaskType.progress,
+              taskModel: _progressTaskList[index],);
+          },
+        ),
       ),
     );
+  }
+
+
+  Future<void> _getProgressTaskList() async {
+    _getProgressTaskInProgress = true;
+    setState(() {});
+
+    NetworkResponse response = await NetworkCaller.getRequest(
+      url: Urls.getProgressTaskUrl,
+    );
+
+    if (response.isSuccess) {
+      List<TaskModel> list = [];
+      for (Map<String, dynamic> jsonData in response.body!['data']) {
+        list.add(TaskModel.fromJson(jsonData));
+      }
+      _progressTaskList = list;
+    } else {
+      showSnackBarMessage(context, response.errorMessage ?? 'Something went wrong');
+    }
+
+    _getProgressTaskInProgress = false;
+    setState(() {});
   }
 }
